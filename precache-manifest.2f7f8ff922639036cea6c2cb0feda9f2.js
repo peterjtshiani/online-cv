1770a4bd6f1ac06bdf8720005a9913eca7e86aee
Peter Tshiani <tshianipeter@gmail.com>
2019-10-14 11:32:32 +0200

    Updates

---
+++ precache-manifest.2f7f8ff922639036cea6c2cb0feda9f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "711aba5e4a36f634e88bcd72f59abbe5",
+    "url": "/online-cv/index.html"
+  },
+  {
+    "revision": "61ab93dddcb6d0798aa5",
+    "url": "/online-cv/static/css/main.26fd8e2c.chunk.css"
+  },
+  {
+    "revision": "ea01d497bf4a470a4e3d",
+    "url": "/online-cv/static/js/2.82d4a155.chunk.js"
+  },
+  {
+    "revision": "61ab93dddcb6d0798aa5",
+    "url": "/online-cv/static/js/main.255c017f.chunk.js"
+  },
+  {
+    "revision": "23865a35d16f72754e52",
+    "url": "/online-cv/static/js/runtime~main.63a87d76.js"
+  },
+  {
+    "revision": "1a1c8e544adef3ee5aa01f2312303ca8",
+    "url": "/online-cv/static/media/icon.1a1c8e54.png"
+  }
+]);
